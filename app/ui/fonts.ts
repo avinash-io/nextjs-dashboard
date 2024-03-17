6b9ebc6aff2dd1eb2d5e18027f473bb259cd1424
Avinash Menon <amenon848@gmail.com>
2024-03-17 16:27:04 +0000

    New File Added

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+//Lusitana
+ //
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+});
